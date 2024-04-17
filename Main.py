d0d134a7bf25ec8161d72b3cfb493f808c4f1d60
smsguy927 <smsguy927@gmail.com>
2024-04-17 04:47:19 -0500

    Generated Test Cases

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from typing import Final
-
+import itertools
 
 def is_different(arr1, arr2):
     for x, y in zip(arr1, arr2):
@@ -8,15 +8,26 @@ def is_different(arr1, arr2):
     return False
 
 
+def calc_gap(left, right):
+    return abs(left - right) - 1
+
+
 def calc_gaps(arr):
     total_gaps = 0
     for i in range(len(arr) - 1):
-        total_gaps += abs(arr[i] - arr[i + 1]) - 1
+        total_gaps += calc_gap(arr[i], arr[i + 1])
     print(f"Arr: {arr} Total gaps: {total_gaps}")
     return total_gaps
 
 
-def find_straights(min_rank, max_rank, board):
+def calc_gap_sizes(arr):
+    gap_sizes = []
+    for i in range(len(arr) - 1):
+        gap_sizes.append(calc_gap(arr[i], arr[i + 1]))
+    return gap_sizes
+
+
+def find_nut_straight(min_rank, max_rank, board):
     straights = []
     if min_rank > max_rank:
         raise ValueError(f"{min_rank} cannot be greater than {max_rank}")
@@ -24,35 +35,41 @@ def find_straights(min_rank, max_rank, board):
         raise ValueError(f"{max_rank} cannot be less than {min_rank}")
     if abs(max_rank - min_rank) < 3:
         return straights
-
+    any_rank: Final[int] = 0
     board.sort(reverse=True)
-    straight_groups = []
-    max_gaps: Final[int] = 2
-    max_index: Final[int] = len(board) - 1
-    straight_size: Final[int] = 3
-    i = 0
-    current_start = 0
-    potential_straight = []
-    while i < len(board):
-        if len(potential_straight) == 0 or potential_straight[-1] != board[i]:
-            potential_straight.append(board[i])
-            if (len(potential_straight) == straight_size and calc_gaps(potential_straight) <= max_gaps and
-                    (len(straight_groups) == 0 or is_different(potential_straight, straight_groups[-1]))):
-                straight_groups.append(potential_straight)
-            if len(potential_straight) == straight_size:
-                potential_straight = []
-                current_start += 1
-                i = current_start - 1
-        i += 1
-    print(straight_groups)
+    board_gaps = calc_gap_sizes(board)
+    print(board_gaps)
+    result = []
+
+    if max(board_gaps) == 0:
+        if board[0] == max_rank:
+            result.append(any_rank)
+        else:
+            result.append(board[0] + 1)
+        result.append(any_rank)
+    elif board_gaps.count(0) == 3 and (board_gaps[0] != 0 or board_gaps[3] != 0):
+        if board_gaps[0] == 0 and board[0] == max_rank:
+            result.append(board[3] - 1)
+        else:
+            result.append(board[0] + 1)
+        result.append(any_rank)
+    elif board_gaps[0] == 0 and board_gaps[1] == 0:
+        pass
+
+    return result
+
+
+def generate_combos(arr, length):
+    return list(itertools.combinations(arr, 5))
 
 
 def main():
     print("Starting program...")
-    try:
-        find_straights(0, 50, [35, 33, 31, 27, 21, 20, 19, 17, 13, 10, 9, 8, 7, 6, 2, 1, 0])
-    except ValueError as e:
-        print(e)
+    all_ranks = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    all_cases = generate_combos(all_ranks, 5)
+    for case in all_cases:
+        print(case)
+        print(find_nut_straight(2, 14, list(case)))
 
 
 main()
